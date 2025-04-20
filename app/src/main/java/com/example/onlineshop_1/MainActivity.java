a3326fc4fff6ca539b82c10644d8aceaff4fa9e0
Himanshu <himanshushrivastwa101@gmail.com>
2025-04-20 18:13:42 +0530

    Initial Commit

--- app/src/main/java/com/example/onlineshop_1/MainActivity.java
+++ app/src/main/java/com/example/onlineshop_1/MainActivity.java
@@ -1,23 +1,14 @@
 package com.example.onlineshop_1;
 
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.onlineshop_1.databinding.ActivitySplashBinding;
-
 public class MainActivity extends AppCompatActivity {
-
-    private ActivitySplashBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        binding = ActivitySplashBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
 
     }
 }
---
+++ app/src/main/java/com/example/onlineshop_1/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.onlineshop_1;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.onlineshop_1.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private ActivitySplashBinding binding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.startBtn.setOnClickListener(v->
+                startActivity(new Intent(SplashActivity.this, MainActivity.class)));
+    }
+}
